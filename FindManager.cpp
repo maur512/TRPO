13f0ee2dcd8e118b3a501550a06db04d55ae43be
Zhur Matsvei <stroiteleistreet@gmail.com>
2022-05-23 09:05:16 +0300

    0.3.2

---
+++ FindManager.cpp
@@ -0,0 +1,8 @@
+#include "FindManager.h"
+
+val::Values findItem (std::vector<val::Values>* value_pointer) {
+
+
+
+    return;
+}
---
+++ FindManager.h
@@ -0,0 +1,3 @@
+#include "includer.h"
+
+val::Values findItem (std::vector<val::Values>*);
